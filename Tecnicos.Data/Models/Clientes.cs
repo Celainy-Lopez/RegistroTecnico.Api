988946239a1db8033931f8065d78146f8110add4
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-11-01 15:46:10 -0400

    Agregar archivos de proyecto.

---
+++ Tecnicos.Data/Models/Clientes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnicos.Data.Models;
+
+public class Clientes
+{
+	[Key]
+	public int ClienteId { get; set; }
+
+	[Required(ErrorMessage = "Ingrese un nombre valido")]
+	[RegularExpression(@"^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$", ErrorMessage = "El nombre del cliente solo debe contener letras.")]
+
+	public string Nombres { get; set; }
+
+	[Required(ErrorMessage = "Ingrese un whatsApp valido")]
+	[RegularExpression(@"^\d{10}$", ErrorMessage = "El número de teléfono debe tener exactamente 10 dígitos.")]
+	public string? WhatsApp { get; set; }
+}
